61e7dcdd15d770fddebcf651ef379b1ed751e3b3
swaroop-12 <swaroop.k.g49@gmail.com>
2024-11-06 19:17:55 +0530

    TreeHashSet

---
+++ TreehashSet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TreehashSet {
+ 
+    public static void main(String[] args) {
+        TreeSet<String>ths =new TreeSet<>();
+        ths.add("Bengaluru");
+        ths.add("Hyderbad");
+        ths.add("Chennai");
+        ths.add("kerla");
+        System.out.println(ths);
+    }
+}
+
+
