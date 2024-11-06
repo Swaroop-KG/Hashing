7c07000c521ad271da25740d9a6b7e71dff70a55
swaroop-12 <swaroop.k.g49@gmail.com>
2024-11-06 19:16:57 +0530

    LinkedHashSet

---
+++ LinkedhashSet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class LinkedhashSet {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Bengaluru");
+        lhs.add("Hyderbad");
+        lhs.add("Chennai");
+        lhs.add("kerla");
+        System.out.println(lhs);
+    }
+}
